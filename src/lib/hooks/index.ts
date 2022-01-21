2ba525c44265c975f9fa2cd1368764ba68e3e92f
mr.pan <1475148345@qq.com>
2022-01-21 15:03:32 +0800

    toast

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,11 +1,17 @@
 import Toast from '../toast/index.vue'
 import { createVNode, render } from 'vue'
+let _container: HTMLElement | null
 export function useToast() {
     return function install(options:any){
-        const container = document.createElement('div')
+        if(_container) document.body.removeChild(_container)
+        _container = document.getElementById('pToast')
+        _container = document.createElement('div')
+        _container.setAttribute('id','pToast')
+
         const vm = createVNode(Toast,options)
 
-        document.body.appendChild(container)
-        render(vm,container)
+        document.body.appendChild(_container)
+        render(vm,_container)
+
     }
 }
