c0f18cc74a24e4f9a86882ea15e243a65f507b0a
root <2210465185@qq.com>
2024-02-16 17:44:29 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import { createApp } from "vue";
+import App from "./App.vue";
+console.log(App);
+import router from "./router/index";
 // import "github-markdown-css";
-import 'pui-vue-pxs/pui.css'
-createApp(App).use(router).mount('#app')
+import "pui-vue-pxs/pui.css";
+createApp(App).use(router).mount("#app");
