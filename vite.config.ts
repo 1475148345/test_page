01d493fffe40c7faefe169d1ab8f9d6b94c8b6fa
root <2210465185@qq.com>
2024-02-16 17:50:20 +0800

    path

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const path = require("path");
 const md = require("vite-plugin-markdown");
 import vitePluginVue from "./src/plugin/vue-custom-blocks-plugin";
 export default defineConfig({
-  plugins: [
-    vue(),
-    md.plugin({ mode: ["html", "vue"]}),
-    vitePluginVue
-  ],
-  base: "./", //打包路径
-  resolve:{
-    alias:{
-      "@": path.resolve(__dirname, "./src"), //设置别名
+    plugins: [vue(), md.plugin({ mode: ["html", "vue"] }), vitePluginVue],
+    base: "./test_page/", //打包路径
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"), //设置别名
+        },
     },
-  },
-  css:{
-    preprocessorOptions:{
-      scss:{
-        additionalData: '@import "@/assets/scss/globalVariable.scss";'
-      }
-    }
-  }
-})
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "@/assets/scss/globalVariable.scss";',
+            },
+        },
+    },
+});
