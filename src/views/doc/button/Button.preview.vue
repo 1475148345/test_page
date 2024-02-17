c019345a962f723aa27acb799a7c639990d31818
root <2210465185@qq.com>
2024-02-17 14:57:54 +0800

    打包1

--- src/views/doc/button/Button.preview.vue
+++ src/views/doc/button/Button.preview.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts">
-import {PButton} from 'pui-vue-pxs'
+import { PButton } from "pui-vue-pxs";
 
-const click=():void=>{
-  console.log(11);
-}
+const click = (): void => {
+    console.log(111);
+};
 </script>
 
 <preview>基础示例</preview>
 <template>
-  <div class="flex flex-w-w ai-c">
-    <p-button @itemAction="click">default</p-button>
-    <p-button type="primary" @itemAction="click">primary</p-button>
-    <p-button type="info">info</p-button>
-    <p-button type="danger">danger</p-button>
-    <p-button type="warning">warning</p-button>
-  </div>
+    <div class="flex flex-w-w ai-c">
+        <p-button @itemAction="click">default</p-button>
+        <p-button type="primary" @itemAction="click">primary</p-button>
+        <p-button type="info">info</p-button>
+        <p-button type="danger">danger</p-button>
+        <p-button type="warning">warning</p-button>
+    </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
